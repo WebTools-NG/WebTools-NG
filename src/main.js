5cfbc164c6d9c456f3309b4d2e023cd25d15931f
knoldesparker <5735792+knoldesparker@users.noreply.github.com>
2020-05-09 12:19:33 +0200

    router, vuex, sidemenu, icons, style frames init

--- src/main.js
+++ src/main.js
@@ -1,8 +1,29 @@
 import Vue from 'vue'
+import VueRouter from "vue-router"
+import Vuex from "vuex"
 import App from './App.vue'
+import router from './router'
+import store from './store'
+
+/*Icons - Styling - Design Frameworks - Sidemenu*/
+import Buefy from 'buefy'
+import 'buefy/dist/buefy.css'
+import VueSidebarMenu from 'vue-sidebar-menu'
+import 'vue-sidebar-menu/dist/vue-sidebar-menu.css'
+import '@fortawesome/fontawesome-free/css/all.css'
+
+
+Vue.use(VueRouter);
+Vue.use(Vuex);
+
+Vue.use(VueSidebarMenu)
+Vue.use(Buefy);
+
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  router,
+  store,
+  render: h => h(App)
 }).$mount('#app')
