e50090cd29147bc9bdb2117cde01165a549ad524
knoldesparker <5735792+knoldesparker@users.noreply.github.com>
2020-05-10 18:46:04 +0200

    Added text to home

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,8 +1,12 @@
 <template>
-  <div>
-    <h1>Home Landing Page</h1>
-    <p>This is the home landing page</p>
-  </div>
+  <section class="section">
+    <h1 class="title is-2">Welcome to WebTools</h1>
+    <h2 class="subtitle">WebTools is a tool that enables the use of tool modules to help with Plex Server management.</h2>
+    <br>
+    <h1 class="title">Tool Modules currently available are:</h1>
+    <h3 class="title is-4">Export</h3>
+    <h4 class="subtitle is-5">Export allows you to export some information about a plex server and files it contains</h4>
+  </section>
 </template>
 
 <script>
