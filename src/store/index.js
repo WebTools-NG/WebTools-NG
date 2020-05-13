b81085bb9de4c2ee2c561faa268e0abbf34d48db
knoldesparker <5735792+knoldesparker@users.noreply.github.com>
2020-05-13 14:06:33 +0200

    state is now persisting

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,15 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import VuexPersistence from 'vuex-persist';
+
 
 Vue.use(Vuex)
+
+const vuexLocal = new VuexPersistence({
+  storage: window.localStorage,
+});
+
+
 
 export default new Vuex.Store({
   state: {
@@ -15,5 +23,6 @@ export default new Vuex.Store({
   actions: {
   },
   modules: {
-  }
+  },
+  plugins: [vuexLocal.plugin]
 })
