5e9a4d092adc1c5d1d867795f49207c1ee81f1ca
UKDTOM <tm@orstrand.dk>
2020-05-24 21:15:35 +0200

    #16

---
+++ src/components/modules/ExportTools/movieLevels.js
@@ -0,0 +1,8 @@
+var levels = ["Level1", "Level2"];
+
+var Level1 = ["Media ID", "Title", "Sort title", 
+                "Studio", "Content Rating", "Year",
+                "Rating", "Summary"];
+
+var Level2 = ["Media ID", "Title", "Sort title"];
+            
