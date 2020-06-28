90724de6ecad21984b4cadedabae2d23fffa60fd
CPSO <5735792+CPSO@users.noreply.github.com>
2020-06-28 15:55:35 +0200

    #7 now displaying langues (sort of)

--- src/components/About.vue
+++ src/components/About.vue
@@ -12,7 +12,7 @@
 
     <div class="container">
   <div class="notification">
-    <p v-for="po in poTranslators" :key="po.name">{{ po.name }}</p>
+    <p v-for="po in poTranslators" :key="po.name">{{ po.name }} {{po.permissions[0].languages}}</p>
   </div>
 </div>
 
@@ -34,6 +34,16 @@ export default {
   this.$store.dispatch('fetchPOEContrib');
   }, computed: {
     poTranslators(){
+
+/*
+      let contip = this.$store.getters.getContrip
+      let list = []
+      contip.forEach(element => {
+        list.push(element.name)
+      });
+      return list
+      */
+
      return this.$store.getters.getContrip
     }
   }
