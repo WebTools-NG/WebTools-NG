0ef7bb82ce3eb5235bdf39de8a3c250d2b737b90
CPSO <5735792+CPSO@users.noreply.github.com>
2020-08-09 17:23:19 +0200

    line fix

--- src/components/About.vue
+++ src/components/About.vue
@@ -8,9 +8,7 @@
     <h1 class="title is-3">{{ $t("Modules.About.TranslationBetaTestersTitle") }}</h1>
     <h2 class="subtitle">{{ $t("Modules.About.TranslationBetaTestersText") }}</h2>
     <div>
-      <span v-for="(po) in poTranslators" :key="po.name">
-        {{ po.name }} </span>
-
+      <span v-for="po in poTranslators" :key="po.name">{{ po.name }} </span>
     </div>
     <br>
     <h1 class="title is-5">{{ $t("Modules.About.PlexPoCredits") }}</h1>
