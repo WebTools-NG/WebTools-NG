f168f27434136fea83e4ca706aae3a74d3b3e501
ukdtom <tm@orstrand.dk>
2020-08-19 14:12:04 +0200

    Version bump, version in title as well

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,5 @@
+process.env.VUE_APP_VERSION = process.env.npm_package_version;
+
 module.exports = {
   pluginOptions: {
     i18n: {
@@ -16,8 +18,18 @@ module.exports = {
         ],
         "linux": {
           "category": "Utility"
-        }
+        }        
       }
     }
   }
+  // ,  
+  // chainWebpack: config => {
+  //   config
+  //       .plugin('html')
+  //       .tap(args => {
+  //         let v = JSON.stringify(require('./package.json').version)
+  //         args[0]['process.env']['VERSION'] = v                    
+  //         return args;
+  //       })
+  // }
 }
