c3f1a276941eee97ef5cf8dc715d9ccbf0376256
UKDTOM <tm@orstrand.dk>
2021-01-09 14:35:17 +0100

    #195 more work

--- src/components/modules/PMS/Settings/settings.vue
+++ src/components/modules/PMS/Settings/settings.vue
@@ -3,7 +3,17 @@
     <div class="col-lg-10 col-md-12 col-xs-12">
         <h1>{{ $t("Modules.PMS.Settings.Settings") }}</h1>
         <p>{{ $t("Modules.PMS.Settings.Description") }}</p>           
-    </div>   
+    </div>
+    <div>
+        <b-form-group id="b-form-group">
+            <b-form-checkbox-group
+                stacked
+                :options="cbOptions"
+                v-model="cbSelected"
+                @change.native="changedOptions">      
+            </b-form-checkbox-group>
+        </b-form-group>
+    </div>
     <div class="d-flex align-items-center">
       <b-form-group id="etLibraryGroup" v-bind:label="$t('Modules.PMS.Settings.SelectSettingsSelection')" label-size="lg" label-class="font-weight-bold pt-0">        
         <!--         
@@ -17,7 +27,7 @@
         <b-form-select 
           v-model="selSection"          
           id="selSection"          
-          :options="selselSectionOptions"
+          :options="selSectionOptions"
           name="selSection">        
         </b-form-select>
       </b-form-group>      
@@ -27,20 +37,78 @@
 
 <script>
     const log = require("electron-log");
-    import {wtutils, wtconfig, dialog} from '../../General/wtutils'
-    import i18n from '../../../../i18n'
+    import {wtutils, wtconfig, dialog} from '../../General/wtutils';
+    import i18n from '../../../../i18n';
+    import store from '../../../../store';
 
-    log, wtutils, wtconfig, dialog, i18n
-    
-
+    wtutils, wtconfig, dialog, i18n, store
     
 
     export default {        
         data() {            
             return {
-                selselSectionOptions: ['ged1', 'ged2']                
+                selSectionOptions: [],
+                selSection : "",
+                cbSelected: [],                
+                cbOptions: [
+                    { text: i18n.t('Modules.PMS.Settings.OnlyHidden'), value: 'OnlyHidden' },
+                    { text: i18n.t('Modules.PMS.Settings.OnlyAdvanced'), value: 'OnlyAdvanced' }                                                         
+                ]                 
             };              
-        },        
+        },
+        created() {
+            log.info("PMS Settings Created");
+            this.serverSelected();
+            this.getServerSettings(); 
+            this.getcbDefaults();           
+        },
+        computed: {
+            selectedServerAddress: function(){
+                return this.$store.getters.getSelectedServerAddress;
+            }
+        },
+        methods: {
+            async serverSelected() {
+                let serverCheck = this.$store.getters.getSelectedServer;
+                if (serverCheck == "none") {
+                    log.debug("serverCheck is none");
+                    this.$bvToast.toast(this.$t("Modules.PMS.ErrorNoServerSelectedMsg"), {
+                        title: this.$t("Modules.PMS.ErrorNoServerSelectedTitle"),
+                        autoHideDelay: 4000,          
+                        solid: true,
+                        variant: 'primary',
+                        toaster: 'b-toaster-bottom-right' 
+                        }
+                    );
+                }
+            },
+            async getServerSettings() {
+                log.debug('Getting Server Settings');
+                await store.dispatch('fetchPMSSettings', {
+                    Token: this.$store.getters.getAuthToken,
+                    Address: this.$store.getters.getSelectedServerAddress}); 
+                log.debug('Options are: ' + JSON.stringify(Object.keys(this.$store.getters.getPMSSettings)))                
+                this.selSectionOptions = Object.keys(this.$store.getters.getPMSSettings);
+            },
+            changedOptions() {
+                log.debug('Updating OnlyHidden Setting');
+                //this.$nextTick(()=>{console.log(this.cbSelected);})
+                for( var cbItem of ["OnlyHidden", "OnlyAdvanced"]){                    
+                    wtconfig.set("PMS." + cbItem, (this.cbSelected.includes(cbItem))) 
+                }
+                this.getServerSettings();
+            },
+            getcbDefaults() {
+                log.debug('Get OnlyHidden Setting');                
+                const cbItems = ["OnlyHidden", "OnlyAdvanced"];
+                for(let i = 0; i < cbItems.length; i++){                     
+                    if (wtconfig.get("PMS." + cbItems[i], true)){
+                        this.cbSelected.push(cbItems[i])
+                    }                    
+                } 
+                log.debug('CBOptions: ' + this.cbSelected)               
+            }
+        }        
     };    
 
 </script>
---
+++ src/store/modules/pms.js
@@ -0,0 +1,101 @@
+import axios from 'axios';
+import {wtconfig, wtutils} from '../../components/modules/General/wtutils';
+
+const log = require('electron-log');
+const {JSONPath} = require('jsonpath-plus');
+
+const state = {
+    settings: {}     
+  };
+
+const mutations = {
+    UPDATE_PMS_SETTINGS(state, payload) {
+        state.settings = payload;
+    }
+};
+
+const getters = {
+    getPMSSettings: state => state.settings
+}
+
+const actions = {
+    async fetchPMSSettings({ commit }, payload) {
+        let header = wtutils.PMSHeader;
+        header['X-Plex-Token'] = payload.Token;        
+        const url = payload.Address + '/:/prefs';
+        await axios({
+                method: 'get',
+                url: url,          
+                headers: header
+            })
+            .then((response) => {             
+              log.debug('Response from fetchPlexServers recieved')              
+              var filteredResult = {}
+              // filteredResult based on hidden or not
+              if (wtconfig.get('PMS.OnlyHidden', true) == true){
+                  log.debug('Show only Hidden settings')                  
+                  filteredResult = JSONPath({path: '$..Setting[?(@.hidden==true)]', json: response.data});                  
+              }
+              else {
+                log.debug('Show non-hidden settings as well')
+                filteredResult = JSONPath({path: '$..Setting', json: response.data})[0];
+              }              
+              if (wtconfig.get('PMS.OnlyAdvanced', true) == true){
+                log.debug('Show only OnlyAdvanced settings')
+                filteredResult = JSONPath({path: '$.[?(@.advanced==true)]', json: filteredResult});                
+              }
+              else {
+                log.debug('Show all settings');                
+              }             
+              var PMSSettings = {};
+              PMSSettings['Undefined'] = {};              
+              filteredResult.forEach(group => {
+                group = JSONPath({path: '$.group', json: group})[0]                
+                if (group !== "") {
+                    PMSSettings[group] = {};
+                }
+              })
+              filteredResult.forEach(element => {
+                var id = JSONPath({path: '$.id', json: element});
+                var jNode = {};
+                jNode['label'] = JSONPath({path: '$.label', json: element})[0];
+                jNode['summary'] = JSONPath({path: '$.summary', json: element})[0];
+                jNode['type'] = JSONPath({path: '$.type', json: element})[0];
+                jNode['default'] = JSONPath({path: '$.default', json: element})[0];
+                jNode['value'] = JSONPath({path: '$.value', json: element})[0];
+                if (JSONPath({path: '$.group', json: element}) == ""){                    
+                    PMSSettings['Undefined'][id] = jNode;                    
+                }
+                else {                    
+                    PMSSettings[JSONPath({path: '$.group', json: element})][id] = jNode;
+                }
+              });              
+              // Remove undefined category, if empty
+              if (Object.keys(PMSSettings['Undefined']).length === 0){                  
+                delete PMSSettings['Undefined'];
+              }
+              commit('UPDATE_PMS_SETTINGS', PMSSettings);
+            })
+            .catch(function (error) {
+              if (error.response) {                  
+                  log.error('fetchPMSSettings: ' + error.response.data)
+                  alert(error.response.data.errors[0].code + " " + error.response.data.errors[0].message)
+              } else if (error.request) {
+                  log.error('fetchPMSSettings: ' + error.request)
+              } else {
+                  log.error('fetchPMSSettings: ' + error.message)
+       }    
+     });
+    }
+};
+
+
+
+const serverModule = {
+    state,
+    mutations,
+    actions,
+    getters
+};
+  
+export default serverModule;
