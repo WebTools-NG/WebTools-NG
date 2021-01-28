a7376fec385f2504379eb98b023527c33ec9cf30
UKDTOM <tm@orstrand.dk>
2021-01-28 23:25:47 +0100

    Fixed path to icon after Electron update

--- src/background.js
+++ src/background.js
@@ -26,7 +26,7 @@ protocol.registerSchemesAsPrivileged([{scheme: 'app', privileges: { secure: true
 
 function createWindow () {
   // Create the browser window.
-  win = new BrowserWindow({ width: 1024, height: 768, icon: __dirname + "../src/assets/WebTools-512.png", webPreferences: {
+  win = new BrowserWindow({ width: 1024, height: 768, icon: __dirname + "/../src/assets/WebTools-512.png", webPreferences: {
     nodeIntegration: true,
     webSecurity: false,
     enableRemoteModule: true
