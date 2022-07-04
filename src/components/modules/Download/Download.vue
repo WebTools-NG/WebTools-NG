f2b0436ef640aaf1c951617ea4e5fe3491bbf4e8
Tommy Mikkelsen <tm@orstrand.dk>
2022-07-04 12:50:44 +0200

    #507 WIP

--- src/components/modules/Download/Download.vue
+++ src/components/modules/Download/Download.vue
@@ -1,16 +1,33 @@
 <template>
-  <b-container fluid>
-    <div>   <!-- Title and desc -->
-      <h2>
-        {{ $t("Modules.Download.Name") }}
-      </h2>
-      <h5>{{ $t("Modules.Download.Description") }}</h5>
+  <div>
+    <div class="text-right">   <!-- Settings button -->
+      <div class="buttons">
+        <!-- Buttons -->
+        <div id="buttons">
+            <b-button-group id="settings">
+              <b-tooltip target="settings" triggers="hover">
+                  {{ $t(`Modules.${this.PageName}.ttSettings`) }}
+              </b-tooltip>
+              <!-- Set class d-none if not used, but keep on page for spacing -->
+              <!-- <button class="btn btn-outline-success d-none" @click="showSettings"><i class="fa fa-cog"></i></button> -->
+              <button class="btn btn-outline-success d-none"><i class="fa fa-cog"></i></button>
+            </b-button-group>
+        </div>
+      </div>
     </div>
-    <br>
-    <br>
+    <b-container fluid>
+      <div>   <!-- Title and desc -->
+        <h2>
+          {{ $t(`Modules.${this.PageName}.Name`) }}
+        </h2>
+        <h5>{{ $t(`Modules.${this.PageName}.Description`) }}</h5>
+      </div>
+      <br>
+      <br>
 
 
-  </b-container>
+    </b-container>
+  </div>
 </template>
 
 <script>
@@ -18,25 +35,21 @@
   //import store from '../../../store';
   //import { wtconfig, wtutils } from '../General/wtutils';
 
-  //i18n, store, wtconfig
-
   const log = require("electron-log");
   export default {
       data() {
         return {
+          PageName: "Download"
         };
   },
   created() {
-    log.info(`[Download.vue] (created) - Download Created`);
+    log.info(`[${this.PageName}.vue] (created) - ${this.PageName} Created`);
   },
   methods: {
+  },
+  watch: {
+  },
+  computed: {
   }
 };
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-#sync-button {
-  margin-left: 1em;
-}
-</style>
