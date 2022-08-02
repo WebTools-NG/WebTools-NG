15b875e94fad7230f1f6d77b59115cc75e75e066
Tommy Mikkelsen <tm@orstrand.dk>
2022-08-02 23:20:24 +0200

    ET and ET Settings

---
+++ src/components/modules/General/wtng-tt.vue
@@ -0,0 +1,45 @@
+<template>
+    <span class="icon is-right" v-bind:id="ttid">
+        <b-tooltip v-bind:target="ttid" triggers="hover">
+            {{ tooltip }}
+        </b-tooltip>
+        <i :style="getStyle" class="ttqmark far fa-question-circle"></i>
+    </span>
+</template>
+
+<script>
+    import i18n from '../../../i18n'
+    export default{
+        props:{
+            tt:{
+                type:String
+            },
+            size:{
+                type:String
+            },
+            color:{
+                type:String,
+                default: 'teal'
+            }
+        },
+        computed:{
+            ttid: function () {
+                return this.tt;
+            },
+            tooltip: function() {
+                return i18n.t(this.tt);
+            },
+            getStyle: function(){
+                return {
+                    color: this.color,
+                    fontSize: this.size
+                }
+            }
+        }
+    }
+</script>
+<style scoped>
+    i.ttqmark{
+        margin-left:5px;
+    }
+</style>
